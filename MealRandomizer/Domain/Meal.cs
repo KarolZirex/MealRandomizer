74ad57ab035868e21e8b434f5a90dc8fdb649d10
KarolZurex <karol.dura1@wp.pl>
2025-02-19 09:37:09 +0100

    push

--- MealRandomizer/Domain/Meal.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MealRandomizer.Domain
-{
-    public class Meal
-    {
-        public string Name { get; set; } = string.Empty;
-        public List<string> Ingredients { get; set; } = new List<string>();
-    }
-}
--- MealRandomizer/Published/wwwroot/script.js
+++
@@ -1,8 +0,0 @@
-﻿function startRoulette(index) {
-    let wheel = document.getElementById("rouletteWheel");
-    if (!wheel) return;
-    let rotations = 5; // Liczba pełnych obrotów
-    let angle = (360 / 5) * index + 360 * rotations;
-    wheel.style.transition = "transform 2s ease-out";
-    wheel.style.transform = `rotate(${angle}deg)`;
-}
